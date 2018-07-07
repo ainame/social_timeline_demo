b1451151fda9875feba77706d0ff7f2ef1d0e2c4
ainame <s.namai.09@gmail.com>
2018-07-07 08:29:36 +0100

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = User.transaction do
+  (1..1000).map do |x|
+    User.create(name: "user#{x}")
+  end
+end
+
+User.transaction do
+  users.each do |user|
+    num = rand(100)
+    puts "#{user.id} - #{num}"
+    num.times do |x|
+      Recipe.create(user: user, title: "recipe #{x} of #{user.name}")
+    end
+  end
+end
+
+users = User.all
+last_id = Recipe.last.id
+User.transaction do
+  users.each do |user|
+    rand(30).times do
+      recipe = Recipe.find(rand(last_id) + 1)
+      BookmarkService.run(user: user, recipe: recipe)
+    end
+    puts "bookmarked: #{user.id}"
+  end
+end
